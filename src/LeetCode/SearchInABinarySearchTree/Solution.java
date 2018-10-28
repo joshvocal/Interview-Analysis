50d2b3eb7e0ec6323472494814e3cf9ca54e31ac
Josh Vocal <joshvocal@gmail.com>
2018-10-28 06:45:18 -0700

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

---
+++ src/LeetCode/SearchInABinarySearchTree/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode.SearchInABinarySearchTree;
+
+import Fundamentals.DataStructures.TreeNode;
+
+public class Solution {
+
+    /*
+     * Time: O(log(n))
+     * We only need to traverse the height of the BST which is log(n)
+     *
+     * Space: O(log(n))
+     * The number of recursive calls is the same as the height of a tree
+     */
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+
+        if (val == root.val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+
+        return null;
+    }
+}
