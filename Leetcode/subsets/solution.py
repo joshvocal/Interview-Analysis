5fbd9d8ac0a4c0e38fdaed6488bb030d6b47452c
Josh Vocal <joshvocal@gmail.com>
2019-01-21 23:31:54 -0800

    add

--- Leetcode/subsets/solution.py
+++ Leetcode/subsets/solution.py
@@ -1,1 +1,24 @@
-print("TODO")
+"""
+Solution 1: DFS
+Time: O(2^n)
+Space: O(2^n)
+"""
+
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.helper(res, [], nums, 0)
+        return res
+
+    def helper(self, res, temp, nums, choice_index):
+        res.append(list(temp))
+
+        for i in range(choice_index, len(nums)):
+            temp.append(nums[i])
+            self.helper(res, temp, nums, choice_index + 1)
+            temp.pop()
+
