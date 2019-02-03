c82d422470a2792bb7a09d5ff72352360ce5f43a
Josh Vocal <joshvocal@gmail.com>
2019-02-03 10:31:33 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/generate_parentheses/solution.py
+++ Leetcode-Python/generate_parentheses/solution.py
@@ -1,1 +1,27 @@
-print("TODO")
+class Solution:
+
+    """
+    Solution 1: DFS (Backtracking)
+    Time:
+    Space: O(2 * n) = O(n)
+    """
+
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+
+        res = []
+        self.backtrack(res, "", 0, 0, n)
+        return res
+
+    def backtrack(self, res, temp, opening, closing, n):
+        if len(temp) == (2 * n):
+            res.append(temp)
+        else:
+            if opening < n:
+                self.backtrack(res, temp + "(", opening + 1, closing, n)
+            if closing < opening:
+                self.backtrack(res, temp + ")", opening, closing + 1, n)
+
