d366b657d11b4fa7fcbc8343e5a864a055d86447
Josh Vocal <joshvocal@gmail.com>
2019-02-03 11:09:35 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/majority_element/solution.py
+++ Leetcode-Python/majority_element/solution.py
@@ -1,1 +1,27 @@
-print("TODO")
+class Solution:
+
+    """
+    Solution 1: Boyer-Moore
+    Time: O(n)
+    Space: O(1)
+    """
+
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = nums[0]
+        count = 0
+
+        for num in nums:
+            if num == candidate:
+                count += 1
+            elif count == 0:
+                candidate = num
+            else:
+                count -= 1
+
+        return candidate
+
+
