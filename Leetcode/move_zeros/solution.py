712c69f7fd9044686fdb0756726ca99ec047b7ba
Josh Vocal <joshvocal@gmail.com>
2019-02-02 16:58:53 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode/move_zeros/solution.py
+++ Leetcode/move_zeros/solution.py
@@ -1,1 +1,23 @@
-print("TODO")
+class Solution:
+
+    """
+    Solution 1: Iterative
+    Time: O(n)
+    Space: O(1)
+    """
+
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        zero_index = 0
+        size = len(nums)
+        
+        for i in range(size):
+            if nums[i] != 0:
+                nums[zero_index] = nums[i]
+                zero_index += 1
+                
+        for i in range(zero_index, size):
+            nums[i] = 0
