3acc0aab9589e3741e267ab3a5b8fc1deed4acfe
Josh Vocal <joshvocal@gmail.com>
2019-02-03 11:00:28 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/delete_node_in_a_linked_list/solution.py
+++ Leetcode-Python/delete_node_in_a_linked_list/solution.py
@@ -1,1 +1,22 @@
-print("TODO")
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+
+    """
+    Solution: 1
+    Time: O(1)
+    Space: O(1)
+    """
+
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
