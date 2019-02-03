eee9488e0ee17ce134a8864e086dc3df1fc46d60
Josh Vocal <joshvocal@gmail.com>
2019-02-03 15:00:15 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/convert_sorted_array_to_binary_search_tree/solution.py
+++ Leetcode-Python/convert_sorted_array_to_binary_search_tree/solution.py
@@ -1,1 +1,33 @@
-print("TODO")
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    """
+    Solution 1: Recursive
+    Time: O(log n)
+    Space: O(log n)
+    """
+
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        head = self.helper(0, len(nums) - 1, nums)
+        return head
+
+    def helper(self, lo, hi, nums):
+        if lo > hi:
+            return None
+
+        mid = (lo + hi) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.helper(lo, mid - 1, nums)
+        node.right = self.helper(mid + 1, hi, nums)
+        return node
+
