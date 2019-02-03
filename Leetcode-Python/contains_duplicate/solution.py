ba9dec5928d5c9017ec185af75ea8cdd68d38770
Josh Vocal <joshvocal@gmail.com>
2019-02-02 17:27:05 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/contains_duplicate/solution.py
+++ Leetcode-Python/contains_duplicate/solution.py
@@ -1,1 +1,22 @@
-print("TODO")
+class Solution:
+
+    """
+    Solution 1: Set
+    Time: O(n)
+    Space: O(n)
+    """
+
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+
+        return False
+
