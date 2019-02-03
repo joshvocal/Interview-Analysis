b2c736f7480de6a1580fe3511e1e403a8b70260a
Josh Vocal <joshvocal@gmail.com>
2019-02-03 10:42:44 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/reverse_linked_list/solution.py
+++ Leetcode-Python/reverse_linked_list/solution.py
@@ -1,1 +1,35 @@
-print("TODO")
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+
+    """
+    Solution 1: Iterative
+    Time: O(n)
+    Space: O(1)
+    """
+
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if not head:
+            return head
+
+        prev = None
+        curr = head
+        next = None
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
+
