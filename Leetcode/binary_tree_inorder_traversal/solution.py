d840a1e0c2cae83a6a5d142eb51ad3493ce9a0a0
Josh Vocal <joshvocal@gmail.com>
2019-02-02 16:48:34 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode/binary_tree_inorder_traversal/solution.py
+++ Leetcode/binary_tree_inorder_traversal/solution.py
@@ -1,14 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-    def fib(self, N):
-        """
-        :type N: int
-        :rtype: int
-        """
-        dp = [0] * (N + 2)
-        dp[0] = 0
-        dp[1] = 1
 
-        for i in range(2, N + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+    """
+    Solution 1: Iterative
+    Time: O(n)
+    Space: O(log n) Worst: O(n)
+    """
 
-        return dp[N]
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        res = []
+        stack = []
+
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = root.pop()
+            res.append(root.val)
+            root = root.left
+
+        return res
+
+    """
+    Solution 2: Recursive
+    Time: O(n)
+    Space: O(log n) Worst: O(n)
+    """
+
+    def inorderTraversal2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        res = []
+
+        if root:
+            res = res + self.inorderTraversal2(root.left)
+            res.append(root.val)
+            res = res + self.inorderTraversal2(root.right)
+
+        return res
