7bf9fcd838d7e1d381ad3edd6c03cd2987b710b6
Josh Vocal <joshvocal@gmail.com>
2019-02-03 18:27:22 -0800

    :running: Lock yourself in a room doing 5 beats a day for 3 summers

--- Leetcode-Python/valid_parentheses/solution.py
+++ Leetcode-Python/valid_parentheses/solution.py
@@ -1,1 +1,28 @@
-print("TODO")
+class Solution:
+
+    """
+    Solution 1: Stack
+    Time: O(n)
+    Space: O(n)
+    """
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for char in s:
+            if char == '(':
+                stack.append(')')
+            elif char == '{':
+                stack.append('}')
+            elif char == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != char:
+                return False
+
+        return len(stack) == 0
+
+
