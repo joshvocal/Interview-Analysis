42d061b991214c8d2a0e986c68193fcee3c53e99
jvocal <jvocal@sfu.ca>
2019-02-18 16:58:27 -0800

    :rocket: Im just waiting on my spaceship

--- Leetcode-Python/rotate_array/solution.py
+++ Leetcode-Python/rotate_array/solution.py
@@ -20,3 +20,21 @@ class Solution:
             nums[hi] = temp
             lo += 1
             hi += 1
+
+"""
+Solution 2: Brute Force
+Time: O(n*k)
+Space: O(1)
+"""
+
+
+class Solution2:
+    def rotate(self, nums, k):
+        n = len(nums)
+
+        for i in range(k):
+            prev = nums[n - 1]
+            for j in range(n):
+                temp = nums[j]
+                nums[j] = prev
+                prev = temp
