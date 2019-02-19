fd87236f6eb3691fff6b8256adeed4b46c4efc77
jvocal <jvocal@sfu.ca>
2019-02-18 16:14:17 -0800

    Add

--- Leetcode-Python/single_number/solution.py
+++ Leetcode-Python/single_number/solution.py
@@ -4,7 +4,6 @@ Time: O(n)
 Space: O(n)
 """
 
-
 class Solution:
     def singleNumber(self, nums):
         """
@@ -21,4 +20,19 @@ class Solution:
 
         for num in s:
             return num
+
+
+"""
+Solution 2: Math
+Time: O(n)
+Space: O(1)
+"""
+
+class Solution2:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2*sum(set(nums)) - sum(nums)
 
--- Leetcode-Python/single_number/test.py
+++ Leetcode-Python/single_number/test.py
@@ -1,5 +1,6 @@
 import unittest
 from solution import Solution
+from solution import Solution2
 
 
 class TestSolution(unittest.TestCase):
@@ -8,30 +9,28 @@ class TestSolution(unittest.TestCase):
         nums = [2, 2, 1]
 
         actual = Solution().singleNumber(nums)
+        actual2 = Solution2().singleNumber(nums)
         expected = 1
 
         self.assertEquals(actual, expected)
+        self.assertEquals(actual2, expected)
 
     def test_example_2(self):
         nums = [4, 1, 2, 1, 2]
 
         actual = Solution().singleNumber(nums)
+        actual2 = Solution2().singleNumber(nums)
         expected = 4
 
         self.assertEquals(actual, expected)
-
-    def test_empty_nums(self):
-        nums = []
-
-        actual = Solution().singleNumber(nums)
-        expected = None
-
-        self.assertEquals(actual, expected)
+        self.assertEquals(actual2, expected)
 
     def test_one_nums(self):
         nums = [1]
 
         actual = Solution().singleNumber(nums)
+        actual2 = Solution().singleNumber(nums)
         expected = 1
 
         self.assertEquals(actual, expected)
+        self.assertEquals(actual2, expected)
