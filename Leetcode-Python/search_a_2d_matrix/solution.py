e7fc15b1c8a47666aa8aa5b6f2a5a5176c2eb1c5
jvocal <jvocal@sfu.ca>
2019-02-19 22:03:13 -0800

    :rocket: Im just waiting on my spaceship

--- Leetcode-Python/search_a_2d_matrix/solution.py
+++ Leetcode-Python/search_a_2d_matrix/solution.py
@@ -18,3 +18,35 @@ class Solution:
                     return True
                 
         return False
+
+
+"""
+Solution 2: Row Binary Search
+Time: O(n*log m)
+Space: O(1)
+"""
+
+class Solution2:
+    def searchMatrix(self, matrix: 'List[List[int]]', target: 'int') -> 'bool':
+        if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        
+        for row in matrix:
+            if target <= row[len(row) - 1]:
+                return self.binary_search(row, target, 0, len(row) - 1)
+        
+        return False
+    
+    def binary_search(self, nums, target, lo, hi):
+        if hi < lo:
+            return False
+            
+        mid = (lo + hi) // 2
+            
+        if target == nums[mid]:
+            return True
+        elif target < nums[mid]:
+            return self.binary_search(nums, target, lo, mid - 1)
+        else:
+            return self.binary_search(nums, target, mid + 1, hi)
+        
