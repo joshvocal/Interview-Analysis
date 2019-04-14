11a266b275b8d949fa6c88ec352cabece386593f
Josh Vocal <joshvocal@gmail.com>
2019-04-14 16:59:36 -0700

    Add

---
+++ AlgoExpert/Python/Arrays/two_number_sum.py
@@ -0,0 +1,42 @@
+'''
+Solution 1: HashMap
+Time: O(n)
+Space: O(n)
+'''
+
+
+def twoNumberSum(arr, target):
+    s = set()
+
+    for num in arr:
+        num_needed = target - num
+        if num_needed in s:
+            return sorted([num_needed, num])
+
+        s.add(num)
+
+    return []
+
+
+'''
+Solution 2: Two Pointers
+Time: O(nlogn)
+Space: O(1)
+'''
+
+def twoNumberSum2(arr, target):
+    arr.sort()
+
+    lo = 0 
+    hi = len(arr) - 1
+
+    while lo < hi:
+        if arr[lo] + arr[hi] == target:
+            return [arr[lo], arr[hi]]
+        elif arr[lo] + arr[hi] > target:
+            hi -= 1
+        else:
+            lo += 1
+
+    return []
+
