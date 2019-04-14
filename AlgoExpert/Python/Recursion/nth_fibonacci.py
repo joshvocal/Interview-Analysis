7399408e3962dee51e666200090972a35a8a3695
Josh Vocal <joshvocal@gmail.com>
2019-04-14 15:31:37 -0700

    Add

---
+++ AlgoExpert/Python/Recursion/nth_fibonacci.py
@@ -0,0 +1,12 @@
+'''
+Solution 1: Recursion
+Time: O(2^n)
+'''
+
+def getNthFib(n):
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return getNthFib(n - 1) + getNthFib(n - 2)
